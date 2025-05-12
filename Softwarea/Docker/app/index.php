aa79b7439fcc7a73dc2b39726dc0ce9e795199ab
aKijn2 <mrkinder20041010@gmail.com>
2025-05-12 09:07:55 +0200

    eguneraketa

--- Softwarea/Docker/app/index.php
+++ Softwarea/Docker/app/index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <p>Hello, World!</p>
     <p>PHP Version: <?php echo phpversion(); ?></p>
@@ -12,5 +14,10 @@
     <p>Server Address: <?php echo $_SERVER['SERVER_ADDR']; ?></p>
     <p>Server Port: <?php echo $_SERVER['SERVER_PORT']; ?></p>
     <p>Request Method: <?php echo $_SERVER['REQUEST_METHOD']; ?></p>
+
+    <a href="Web/interface/bezeroak/froga.html">Go to bezeroak</a>
+    <a href="Web/interface/gidariak/froga.html">Go to gidariak</a>
+
 </body>
+
 </html>
