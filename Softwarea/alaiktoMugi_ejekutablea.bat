5f47a1e61c3737c5aa85a621ab55501713dbcfc5
aKijn2 <mrkinder20041010@gmail.com>
2025-05-28 09:09:18 +0200

    zuzenketak

--- Softwarea/alaiktoMugi_ejekutablea.bat
+++ Softwarea/alaiktoMugi_ejekutablea.bat
@@ -1,3 +1,25 @@
 @echo off
-"C:\Users\ikerh\Downloads\openjdk-24.0.1_windows-x64_bin\jdk-24.0.1\bin\java.exe" -jar "C:\Users\ikerh\Desktop\Proiektua\aplikazioa.jar"
+REM Helburuko direktorioa ezarri
+set "HELBURU_DIR=%~dp0alaiktoMugi"
+
+REM Direktorioa existitzen den egiaztatu
+if not exist "%HELBURU_DIR%" (
+    echo Direktiorioa ez da aurkitu: %HELBURU_DIR%
+    pause
+    exit /b 1
+)
+
+REM Direktoriora joan
+cd /d "%HELBURU_DIR%"
+
+REM JAR fitxategia existitzen den egiaztatu
+if not exist "alaiktoMugi.jar" (
+    echo JAR fitxategia ez da aurkitu: alaiktoMugi.jar
+    pause
+    exit /b 1
+)
+
+REM Java aplikazioa abiarazi
+java -jar alaiktoMugi.jar
+
 pause
